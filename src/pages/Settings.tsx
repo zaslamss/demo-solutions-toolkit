2b984927857e0ebd7a265e748f4c6df7b6e32615
Zara Aslam <zara.aslam@smartsheet.com>
2025-08-28 22:29:33 -0400

    Link the google oauth button with actual url

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,13 +1,13 @@
 import { Button, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const Settings = () => {
     const googleOAuthUrl = import.meta.env.VITE_GOOGLE_OAUTH_URL
 
-    console.log(googleOAuthUrl);
     return (
         <Container>
             <h4 className="mb-3 fw-bold text-primary">Account Settings</h4>
-            <Button variant="primary">Connect to Google OAuth</Button>
+            <Link style={{textDecoration: 'none'}} to={googleOAuthUrl}><Button variant="primary">Connect to Google OAuth</Button></Link>
         </Container>
     )
 }
