0b71ddea64301f00d82dd984978e8d114dd036a0
Zara Aslam <zara.aslam@smartsheet.com>
2025-08-28 21:43:53 -0400

    Add environment variable for google oauth link

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,6 +1,9 @@
 import { Button, Container } from "react-bootstrap";
 
 export const Settings = () => {
+    const googleOAuthUrl = process.env.REACT_APP_GOOGLE_OAUTH_URL;
+
+    console.log(googleOAuthUrl);
     return (
         <Container>
             <h4 className="mb-3 fw-bold text-primary">Account Settings</h4>
